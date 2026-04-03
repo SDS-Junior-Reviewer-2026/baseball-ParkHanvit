f5a3e539c5ed33e437731efc03e3e2e2ab8dd5fe
epdlqlc <davidgoparkid369@gmail.com>
2026-04-03 14:09:15 +0900

    [feature] 73p

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -57,8 +57,11 @@ public class GameTest {
     }
 
     @Test
-    public void 볼만_있을_경우_0_strike_1_ball() {
-
+    public void returnUnsolvedResultIfSomeMatchedNumber() {
+        generateQuestion("123");
+        assertMatchedNumber(game.guess("120"), false, 2, 0);
+        assertMatchedNumber(game.guess("061"), false, 0, 1);
+        assertMatchedNumber(game.guess("136"), false, 1, 1);
     }
 
     @Test
