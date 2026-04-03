35645c310d2a22afae5ae6f75f8986dfc39c8cdb
epdlqlc <davidgoparkid369@gmail.com>
2026-04-03 13:34:20 +0900

    [feature] 46p

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -23,6 +23,7 @@ public class GameTest {
         assertIllegalArgument(null);
         assertIllegalArgument("12");
         assertIllegalArgument("1234");
+        assertIllegalArgument("12s");
     }
 
 
